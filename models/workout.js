6ceab883307a0d8af3eac8bd0a4bcb784ad93e3f
Matt Randolph <mttrndlph@gmail.com>
2021-04-06 18:12:16 -0400

    adding heroku connection, models, routes to come

---
+++ models/workout.js
@@ -0,0 +1,18 @@
+// Dependencies //
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+    date: {
+      type: Date,
+      default: new Date().setDate(new Date().getDate())
+    },
+    exercises: Array
+}
+);
+  
+  const workout = mongoose.model("Workout", workoutSchema);
+
+  // Export the database//
+  module.exports = workout;
--- server.js
+++ server.js
@@ -1,19 +1,25 @@
-const express = require("express");
-const logger = require("morgan");
-const mongoose = require("mongoose");
+const express = require('express');
+const logger = require('morgan');
+const mongoose = require('mongoose');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.use(logger("dev"));
-
+app.use(logger('dev'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
+app.use(require('./routes/api'));
+app.use(require('./routes/html'));
 
-app.use(express.static("public"));
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts", { useNewUrlParser: true });
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
