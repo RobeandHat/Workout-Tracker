d0e90c82921a8bcaf7a1382302275dc440fe592c
Matt Randolph <mttrndlph@gmail.com>
2021-04-07 12:04:09 -0400

    routes added, model slightly changed, still bugs

--- models/workout.js
+++ models/workout.js
@@ -3,16 +3,20 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const workoutSchema = new Schema({
+// Workout Schema //
+
+const workoutSchema = new Schema(
+  {
     date: {
       type: Date,
-      default: new Date().setDate(new Date().getDate())
+      default: new Date()
     },
     exercises: Array
 }
 );
   
-  const workout = mongoose.model("Workout", workoutSchema);
+const workout = mongoose.model("Workout", workoutSchema);
 
-  // Export the database//
-  module.exports = workout;
+ // Export the database//
+ 
+ module.exports = workout;
--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
-// app.use(require('./routes/api'));
-// app.use(require('./routes/html'));
+app.use(require('./routes/api'));
+app.use(require('./routes/html'));
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
   useNewUrlParser: true,
